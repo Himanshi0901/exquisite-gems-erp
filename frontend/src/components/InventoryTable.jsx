20fe3205a9aedda56719a0d5dcc5821d5b1ab4d2
Exquisite Gems <himanshi9104@gmail.com>
2026-05-19 17:13:02 +0530

    change

--- frontend/src/components/InventoryTable.jsx
+++ frontend/src/components/InventoryTable.jsx
@@ -246,7 +246,7 @@ function InventoryTable({
                           Days Left
                         </p>
 
-                        <p className="text-[20px] md:text-[28px] font-black text-red-600 leading-none tracking-tight mt-2 break-words text-center">
+                        <p className="text-[15px] md:text-[20px] font-black text-red-600 leading-none tracking-tight mt-2 break-words text-center">
                           {
                             remaining.text
                           }
